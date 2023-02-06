305df83a7d8b36c6b94cfbd81ffb537d93f367ea
MrSupertash <mrsupertash@gmail.com>
2023-02-06 15:46:56 +0100

    exercise done

--- Class 26/objects-constructor-fun/js/main.js
+++ Class 26/objects-constructor-fun/js/main.js
@@ -1,1 +1,18 @@
 //Create a constructor with 4 properties and 3 methods
+
+function MakePizza(sauce, toppings, crust, cheese) {
+    this.sauce = sauce;
+    this.toppings = toppings;
+    this.crust = crust;
+    this.cheese = cheese;
+
+    this.burnMouth = function() {
+        console.log("FTFTSTFASTSTAAAAAAAHH!");
+    };
+    this.frisbee = function() {
+        console.log('Wheeeeeeeeeee!');
+    };
+    this.prepTime = function() {
+        console.log('Super fast!');
+    };
+}
