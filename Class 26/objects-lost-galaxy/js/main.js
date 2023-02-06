18230891541c0096ea23e5df414978e1af386437
MrSupertash <mrsupertash@gmail.com>
2023-02-06 15:16:25 +0100

    first iteration

--- Class 26/objects-lost-galaxy/js/main.js
+++ Class 26/objects-lost-galaxy/js/main.js
@@ -1,1 +1,18 @@
-//Create a dog object that has four properties and three methods
+//Create a pizza object that has four properties and three methods
+
+let pizza = {
+    toppings: ['pepperoni', 'ham', 'shrooms', 'onions'],
+    crust: 'NYC',
+    size: 'medium',
+    sauce: 'tomato',
+    burnMouth: function() {
+        console.log('FTZZTHHIDSFSIAAAAAAAA');
+    },
+    frisbee: function() {
+        console.log('Wheeeeeeeeeeee');
+    },
+    prepTime: function() {
+        console.log('Very quick!')
+    }
+}
+
